d3bb8d477c123f3391a87b8e5c6d848998b0a4c9
Rahul Singh <85427061+rahulrely@users.noreply.github.com>
2024-10-01 17:50:11 +0530

    updated

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path : "./env"
+})
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server Running on PORT : ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB CONNECT FAILED",err);
+})
+
+
+
+
+
+
+// import express from "express";
+// const app = express()
+
+// (async () =>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         app.on("error : ",(error)=>{
+//             console.log("Error :",error);
+//             throw error;            
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`App is listen on Port : ${process.env.PORT}`);
+//         })
+//     } catch (error) {
+//         console.log("ERROR : ",error);
+//         throw error;
+//     }
+// })()
